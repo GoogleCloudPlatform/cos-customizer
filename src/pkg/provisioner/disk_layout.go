e228a72bea3b53630fda609c2e7da0d9a4d5da18
Robert Kolchmeyer <rkolchmeyer@google.com>
2021-03-03 15:03:06 -0800

    provisioner: Implement behavior for relocating partitions
    This behavior is implemented in the same way as startup.sh. We install a
    systemd unit to run at shutdown time, after relevant partitions are
    unmounted. This unit relocates partitions according to requirements.
    
    Similar to startup.sh, we use the handle_disk_layout.bin program to
    repartition the boot disk. We embed this program into the Go package
    using Go 1.16's new embed behavior to distribute handle_disk_layout.bin
    statically with the provisioner program. This makes deployment much
    easier.
    
    To use Go's embed behavior, I needed to upgrade our Go version to 1.16,
    so I did that in the WORKSPACE file.
    
    ./run_tests.sh passes.

--- src/pkg/provisioner/disk_layout.go
+++ src/pkg/provisioner/disk_layout.go
@@ -15,17 +15,24 @@
 package provisioner
 
 import (
+	_ "embed"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 
+	"golang.org/x/sys/unix"
+
 	"github.com/GoogleCloudPlatform/cos-customizer/src/pkg/tools/partutil"
 	"github.com/GoogleCloudPlatform/cos-customizer/src/pkg/utils"
 )
+
+//go:embed _handle_disk_layout.bin
+var handleDiskLayoutBin []byte
 
 func switchRoot(deps Deps, runState *state) (err error) {
 	if !runState.data.Config.BootDisk.ReclaimSDA3 {
@@ -91,6 +98,100 @@ func shrinkSDA3(deps Deps, runState *state) error {
 	return ErrRebootRequired
 }
 
+func setupOnShutdownUnit(deps Deps, runState *state) (err error) {
+	if err := mountFunc("", filepath.Join(deps.RootDir, "tmp"), "", unix.MS_REMOUNT|unix.MS_NOSUID|unix.MS_NODEV, ""); err != nil {
+		return fmt.Errorf("error remounting /tmp as exec: %v", err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(deps.RootDir, "tmp", "handle_disk_layout.bin"), handleDiskLayoutBin, 0744); err != nil {
+		return err
+	}
+	data := fmt.Sprintf(`[Unit]
+Description=Run after everything unmounted
+DefaultDependencies=false
+Conflicts=shutdown.target
+Before=mnt-stateful_partition.mount usr-share-oem.mount
+After=tmp.mount
+
+[Service]
+Type=oneshot
+RemainAfterExit=true
+ExecStart=/bin/true
+ExecStop=/bin/bash -c '/tmp/handle_disk_layout.bin /dev/sda 1 8 "%s" "%t" 2>&1 | sed "s/^/BuildStatus: "'
+TimeoutStopSec=600
+StandardOutput=tty
+StandardError=tty
+TTYPath=/dev/ttyS2
+`, runState.data.Config.BootDisk.OEMSize, runState.data.Config.BootDisk.ReclaimSDA3)
+	if err := ioutil.WriteFile(filepath.Join(deps.RootDir, "etc/systemd/system/last-run.service"), []byte(data), 0664); err != nil {
+		return err
+	}
+	systemd := systemdClient{systemctl: deps.SystemctlCmd}
+	if err := systemd.start("last-run.service", []string{"--no-block"}); err != nil {
+		return err
+	}
+	// journald needs to be stopped in order for the stateful partition to be
+	// unmounted at shutdown. We need the stateful partition to be unmounted so
+	// that disk repartitioning can occur.
+	if err := systemd.stopJournald(deps.RootDir); err != nil {
+		return err
+	}
+	return nil
+}
+
+func relocatePartitions(deps Deps, runState *state) error {
+	if !runState.data.Config.BootDisk.ReclaimSDA3 && runState.data.Config.BootDisk.OEMSize == "" {
+		log.Println("ReclaimSDA3 is not set, OEM resize not requested, not relocating partitions")
+		return nil
+	}
+	device := filepath.Join(deps.RootDir, "dev", "sda")
+	sda3Start, err := partutil.ReadPartitionStart(device, 3)
+	if err != nil {
+		return err
+	}
+	sda3Size, err := partutil.ReadPartitionSize(device, 3)
+	if err != nil {
+		return err
+	}
+	sda3End := sda3Start + sda3Size - 1
+	if runState.data.Config.BootDisk.OEMSize != "" {
+		// Check if OEM partition is after sda3; if so, then we're done
+		oemStart, err := partutil.ReadPartitionStart(device, 8)
+		if err != nil {
+			return err
+		}
+		if oemStart > sda3End {
+			log.Println("OEM resize requested, OEM appears to be relocated after sda3. Partition relocation is complete")
+			return nil
+		}
+	} else {
+		// Check two things:
+		// 1. sda3 is minimal
+		// 2. Stateful partition is located after sda3
+		//
+		// If both are true, we are done.
+		minimal, err := partutil.IsPartitionMinimal(device, 3)
+		if err != nil {
+			return err
+		}
+		statefulStart, err := partutil.ReadPartitionStart(device, 1)
+		if err != nil {
+			return err
+		}
+		if minimal && statefulStart == sda3End+1 {
+			log.Println("ReclaimSDA3 is set, sda3 appears to have been reclaimed. Partition relocation is complete")
+			return nil
+		}
+	}
+	// Partition relocation must be done. Prepare for disk relocation to happen on
+	// the next reboot
+	log.Println("Partition relocation is required. Preparing for partition relocation to occur on the next reboot")
+	if err := setupOnShutdownUnit(deps, runState); err != nil {
+		return err
+	}
+	log.Println("Reboot required to relocate partitions")
+	return ErrRebootRequired
+}
+
 // repartitionBootDisk executes all behaviors related to repartitioning the boot
 // disk. Most of these behaviors require a reboot. To keep reboots simple (e.g.
 // we don't want to initiate a reboot when deferred statements are unresolved),
@@ -101,6 +202,9 @@ func repartitionBootDisk(deps Deps, runState *state) error {
 		return err
 	}
 	if err := shrinkSDA3(deps, runState); err != nil {
+		return err
+	}
+	if err := relocatePartitions(deps, runState); err != nil {
 		return err
 	}
 	return nil
