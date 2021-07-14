a812c9b73f76779c4759513f921667f2049e9f25
Robert Kolchmeyer <rkolchmeyer@google.com>
2021-07-14 10:42:51 -0700

    Update cos-gpu-installer to latest version
    Fixes an issue where GPU builds fail if the standalone toolchain
    includes hard links to the rust standard library.

--- src/cmd/cos_customizer/install_gpu.go
+++ src/cmd/cos_customizer/install_gpu.go
@@ -39,7 +39,7 @@ import (
 
 const (
 	gpuScript          = "install_gpu.sh"
-	installerContainer = "gcr.io/cos-cloud/cos-gpu-installer:v20210319"
+  installerContainer = "gcr.io/cos-cloud/cos-gpu-installer:v20210714"
 )
 
 // TODO(b/121332360): Move most GPU functionality to cos-gpu-installer
